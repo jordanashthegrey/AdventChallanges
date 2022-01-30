9d7e05b96b361e7ae3b39c00035563d540b59542
Jordan Gijsbertha <jordanashmir@gmail.com>
2022-01-30 21:44:42 +0100

    Assignment 1 day 3 completed in one try lesgoo

---
+++ day 3/assignment_1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+
+int main()
+{
+    std::ifstream inFS;
+    inFS.open("input.txt");
+    std::string bit;
+    std::vector<std::string> bitList;
+    std::vector<int> gamma;
+    std::vector<int> epsilon;
+    int countZero = 0;
+    int countOne = 0;
+    int counter = 0;
+
+    while (!inFS.eof())
+    {
+        inFS >> bit;
+        bitList.push_back(bit);
+    }
+
+    for (int j = 0; j < 12; j++)
+    {
+        for (int i = 0; i < bitList.size(); i++)
+        {
+            if (bitList.at(i)[j] == '1')
+            {
+                countOne += 1;
+            }
+            else
+            {
+                countZero += 1;
+            }
+            // std::cout << bitList.at(i)[0] << std::endl;
+        }
+        if (countOne > countZero)
+        {
+            gamma.push_back(1);
+            epsilon.push_back(0);
+        }
+        else
+        {
+            gamma.push_back(0);
+            epsilon.push_back(1);
+        }
+        countOne = 0;
+        countZero = 0;
+        // std::cout << countOne << std::endl;
+        // std::cout << countZero << std::endl;
+    }
+
+    for (int i = 0; i < gamma.size(); i++) {
+        std::cout << gamma.at(i);
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < epsilon.size(); i++) {
+        std::cout << epsilon.at(i);
+    }
+
+
+    return 0;
+}
